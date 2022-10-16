1361bbc0ab039d8b6dd1c60575635a4cd8161fc0
Felipe Sales <felipefsp.dev@gmail.com>
2022-10-16 13:38:25 -0300

    renomeação da página inicial

--- paginas-componentes/header.js
+++ paginas-componentes/header.js
@@ -8,7 +8,7 @@ class Header extends HTMLElement {
       
   <header class="bg-color-c12">
     <div class="header container">
-      <a href="./home.html"><img src="img/bikcraft.svg" alt="Bikcraft" width="136" height="32"></a>
+      <a href="./index.html"><img src="img/bikcraft.svg" alt="Bikcraft" width="136" height="32"></a>
       <nav class="menu" aria-label="Navegação Primária">
         <ul class="font-1-m color-c0">
         <li><a href="./bicicletas.html">Bicicletas</a></li>
